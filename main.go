16adf288a14b57fc8fa750f699ca89db8b2c1250
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-07-05 11:34:32 +0900

    Todoの作成

--- main.go
+++ main.go
@@ -15,25 +15,27 @@ func main() {
 		log.Println("test")
 	*/
 
-	/*
-		fmt.Println(models.Db)
-		u := &models.User{}
-		u.Name = "test"
-		u.Email = "test@example.com"
-		u.PassWord = "testtest"
-		fmt.Println(u)
-
-		u.CreateUser()
-	*/
-	u, _ := models.GetUser(1)
-	fmt.Println(u)
-	u.Name = "test2"
-	u.Email = "test2@example.com"
-	u.UpdateUser()
-	u, _ = models.GetUser(1)
+	fmt.Println(models.Db)
+	u := &models.User{}
+	u.Name = "test"
+	u.Email = "test@example.com"
+	u.PassWord = "testtest"
 	fmt.Println(u)
 
-	u.DeleteUser()
-	u, _ = models.GetUser(1)
-	fmt.Println(u)
+	u.CreateUser()
+
+	//u, _ := models.GetUser(1)
+	//fmt.Println(u)
+	//u.Name = "test2"
+	//u.Email = "test2@example.com"
+	//u.UpdateUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
+	//
+	//u.DeleteUser()
+	//u, _ = models.GetUser(1)
+	//fmt.Println(u)
+
+	user, _ := models.GetUser(2)
+	user.CreateTodo("First todo")
 }
