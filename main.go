c260d9e3e057e1676855b71db48afa17db2842cf
Yuki Hosokawa <y.hosokawa.r@gmail.com>
2022-07-06 11:23:22 +0900

    Start Server

--- main.go
+++ main.go
@@ -1,54 +1,14 @@
 package main
 
-import "hello/app/models"
+import (
+	"fmt"
+	"hello/app/controllers"
+	"hello/app/models"
+)
 
 func main() {
-	//fmt.Println(models.Db)
-	//u := &models.User{}
-	//u.Name = "test"
-	//u.Email = "test@example.com"
-	//u.PassWord = "testtest"
-	//fmt.Println(u)
-	//
-	//u.CreateUser()
+	fmt.Println(models.Db)
 
-	//u, _ := models.GetUser(1)
-	//fmt.Println(u)
-	//u.Name = "test2"
-	//u.Email = "test2@example.com"
-	//u.UpdateUser()
-	//u, _ = models.GetUser(1)
-	//fmt.Println(u)
-	//
-	//u.DeleteUser()
-	//u, _ = models.GetUser(1)
-	//fmt.Println(u)
-
-	/*
-		user, _ := models.GetUser(2)
-		user.CreateTodo("First todo")
-	*/
-
-	//t, _ := models.GetTodo(1)
-	//fmt.Println(t)
-
-	//user, _ := models.GetUser(3)
-	//user.CreateTodo("Third todo")
-
-	//todos, _ := models.GetTodos()
-	//for _, v := range todos {
-	//	fmt.Println(v)
-	//}
-	//user2, _ := models.GetUser(2)
-	//todos, _ := user2.GetTodosByUser()
-	//for _, v := range todos {
-	//	fmt.Println(v)
-	//}
-	//t, _ := models.GetTodo(1)
-	//t.Content = "update todo"
-	//t.UpdateTodo()
-
-	t, _ := models.GetTodo(3)
-	t.DeleteTodo()
+	controllers.StartMainServer()
 
 }
